60181951c394a142c47bb4e5c40758134ce2015f
David Macara Brown <davidmacarabrown@gmail.com>
2021-07-27 19:38:14 +0100

    seven UI tests passing

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -32,4 +32,40 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '4');
   })
+
+  it('should be able to handle floating point numbers', () => {
+    cy.get('#number5').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number1').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '0.5')
+  })
+
+  it('should be able to handle negative numbers', () => {
+    cy.get('#number1').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number7').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-6')
+
+  })
+
+  it('should be able to handle very large numbers', () => {
+    cy.get('#number9').click();
+    cy.get('#number8').click();
+    cy.get('#number7').click();
+    cy.get('#number6').click();
+    cy.get('#number5').click();
+    cy.get('#number4').click();
+    cy.get('#number3').click();
+    cy.get('#number2').click();
+    cy.get('#number1').click();
+    cy.get('#operator-add').click();
+    cy.get('#number9').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', "987654330")
+  })
+
+
 })
