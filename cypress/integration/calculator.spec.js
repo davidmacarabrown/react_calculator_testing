6708bcdf1360e8244e50798b9cf19922a03caa2e
David Macara Brown <davidmacarabrown@gmail.com>
2021-07-27 19:05:40 +0100

    three UI tests passing

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -13,4 +13,23 @@ describe("Calculator", () => {
     cy.get('#number5').click();
     cy.get('.display').should('contain', '55')
   })
+
+  it('arithmetical operators should update the display with the result of their operation', () => {
+
+    cy.get('#number6').click();
+    cy.get('#operator-add').click();
+    cy.get('#number6').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '12');
+  })
+
+  it('should be able to handle chained operations', () => {
+    cy.get('#number6').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number2').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number3').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '4');
+  })
 })
